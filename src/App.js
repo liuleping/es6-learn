a26460a888c5657c92fa7fbcfcb46401168f7917
liuleping0114 <liuleping0114@github.com>
2018-07-03 21:16:54 +0800

    frist commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+function Demo1() {
+  return (
+    <div></div>
+  );
+}
+
+export default class App extends Component { // eslint-disable-line
+  render() {
+    return (
+      <div>
+          <Demo1 />
+      </div>
+    );
+  }
+}
